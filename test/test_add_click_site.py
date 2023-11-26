310b010294467eda93a573e506170520d075473f
Vadym QA <vadim.andriienko@sweetstackdigital.com>
2023-11-26 15:12:02 +0200

    Add hm

---
+++ test/test_add_click_site.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from conftest import browser
+
+
+def test_click_all_menu_items(browser):
+    browser.get("https://the-internet.herokuapp.com/floating_menu#home")
+    wait = WebDriverWait(browser, 7)
+
+    menu_item_locators = [
+        (By.XPATH, "//a[text()='Home']"),
+        (By.XPATH, "//a[text()='News']"),
+        (By.XPATH, "//a[text()='Contact']"),
+        (By.XPATH, "//a[text()='About']")
+    ]
+    for menu_item_locator in menu_item_locators:
+        menu_item = wait.until(EC.presence_of_element_located(menu_item_locator))
+        menu_item.click()
+        assert "floating_menu" in browser.current_url.lower()
+
